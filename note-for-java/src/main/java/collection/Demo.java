e27ad9e7c3b591e2b2b4aeab5ba8d82d33335a6b
YuanMosnter <monster_best@163.com>
2026-01-16 15:00:11 +0800

    遍历数组的三种方式

--- note-for-java/src/main/java/collection/Demo.java
+++ note-for-java/src/main/java/collection/Demo.java
@@ -1,5 +1,6 @@
 package collection;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Demo {
@@ -16,9 +17,29 @@ public class Demo {
          * 遍历
          */
         List<String> list1 = List.of("apple", "pear", "banana");
+        /*方法一
+         *
+         */
         for (int i = 0; i < list1.size(); i++) {
             System.out.println(list1.get(i));
         }
+
+        /*
+        方法 使用Iterator进行访问
+         */
+
+        for (Iterator<String> it = list1.iterator(); it.hasNext();) {
+         String s = it.next();
+            System.out.println(s);
+        }
+
+        for (String s : list1) {
+
+            System.out.println(s);
+        }
+
+
+
     }
 
 }
